f68017fb14693a006f95d840aa977fd7a5c3939d
นงนภัส สำแดงเดช <nongnapat.som@medicine.edu>
2017-09-29 16:03:30 +0700

    test

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -23,7 +23,7 @@ class LineBotController extends Controller
     	$response = $bot->replyMessage($replyToken, $textMessageBuilder);
         
 		$SKID = 1;
-		$textMessageBuilder2 = new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder(1, 1,100);
+		$textMessageBuilder2 = new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder(2,1);
 		
 		$response2 = $bot->pushMessage($userId, $textMessageBuilder2);
 		
