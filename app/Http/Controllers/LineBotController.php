b9bc7d3cd9bcae8245008850ab89d896c69d1bce
นงนภัส สำแดงเดช <nongnapat.som@medicine.edu>
2017-10-05 09:53:42 +0700

    test

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -56,6 +56,7 @@ class LineBotController extends Controller
 			$img_url = "https://cdn.shopify.com/s/files/1/0379/7669/products/sampleset2_1024x1024.JPG?v=1458740363";
 			$button = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder("button text", "description", $img_url, $actions);
 			$outputText = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("this message to use the phone to look to the Oh", $button);
+			$response = $bot->replyMessage($replyToken, $outputText);
 			break;
    	 	default: 
 
